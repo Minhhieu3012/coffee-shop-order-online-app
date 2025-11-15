7b09a8fab646a010e22a0f7aaf6478fec9436193
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-11-16 00:04:29 +0700

    fix: correct build config syntax errors

--- CustomerMobile/app/build.gradle.kts
+++ CustomerMobile/app/build.gradle.kts
@@ -2,15 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-
-    id("com.google.gms.google-services")
+    alias(libs.plugins.google.services)
 }
 
 android {
     namespace = "vn.edu.ut.hieupm9898.customermobile"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "vn.edu.ut.hieupm9898.customermobile"
