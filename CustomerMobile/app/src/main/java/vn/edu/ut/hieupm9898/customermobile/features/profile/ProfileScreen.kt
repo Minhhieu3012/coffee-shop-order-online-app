af8632fcb4a336c1880e04534677abbef05718fa
LDT1005 <laithanh4869@gmail.com>
2025-11-19 22:22:56 +0700

    ProfileScreen

--- CustomerMobile/app/src/main/java/vn/edu/ut/hieupm9898/customermobile/features/profile/ProfileScreen.kt
+++ CustomerMobile/app/src/main/java/vn/edu/ut/hieupm9898/customermobile/features/profile/ProfileScreen.kt
@@ -1,2 +1,25 @@
 package vn.edu.ut.hieupm9898.customermobile.features.profile
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Màn hình Hồ sơ (TBD)")
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen(navController = rememberNavController())
+}
