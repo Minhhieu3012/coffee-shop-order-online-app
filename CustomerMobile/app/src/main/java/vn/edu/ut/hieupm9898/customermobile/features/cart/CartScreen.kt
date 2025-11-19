880fb50f7fe9a91a9a61fa48569d672ac2dff21a
LDT1005 <laithanh4869@gmail.com>
2025-11-19 22:20:51 +0700

    cartscreen

--- CustomerMobile/app/src/main/java/vn/edu/ut/hieupm9898/customermobile/features/cart/CartScreen.kt
+++ CustomerMobile/app/src/main/java/vn/edu/ut/hieupm9898/customermobile/features/cart/CartScreen.kt
@@ -1,2 +1,24 @@
 package vn.edu.ut.hieupm9898.customermobile.features.cart
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun CartScreen(navController: NavController) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Màn hình Giỏ hàng (TBD)")
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CartScreenPreview() {
+    CartScreen(navController = rememberNavController())
+}
