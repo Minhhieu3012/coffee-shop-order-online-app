cf884e3bf26af6abbb5c0f63b4f55bdb753d4359
ghu0101exe <g.huy0102005@gmail.com>
2025-11-24 22:53:31 +0700

    update: merge

--- CustomerMobile/app/src/main/java/vn/edu/ut/hieupm9898/customermobile/BaseApplication.kt
+++
@@ -1,9 +0,0 @@
-package vn.edu.ut.hieupm9898.customermobile
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class BaseApplication : Application() {
-    // Hilt sẽ tự động sinh code ở đây, bạn không cần viết gì thêm
-}
